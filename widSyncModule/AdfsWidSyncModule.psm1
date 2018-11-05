ae4b0f6adca65a2f76a64218d7e0376870952bfd
Madhav Patel <patel.v.madhav@gmail.com>
2018-11-05 16:17:04 -0500

    Revert "Replace Write-Host with Write-Output to allow invoking (#50)" (#51)
    This reverts commit c05a381203ce325145fc3b5ade2cf2c7d1e4e1ba.

--- widSyncModule/AdfsWidSyncModule.psm1
+++ widSyncModule/AdfsWidSyncModule.psm1
@@ -29,7 +29,7 @@ function Reset-AdfsWidServiceStateSummarySerialNumbers
     $sqlcmd.CommandText = $update;
     $sqlcmd.CommandTimeout = 600000;
     $rowsAffected = $sqlcmd.ExecuteNonQuery()
-    Write-Output $rowsAffected "rows have been affected by the reset of SerialNumber column"
+    Write-Host $rowsAffected "rows have been affected by the reset of SerialNumber column"
 } 
 
 function Invoke-WidSync
@@ -41,7 +41,7 @@ function Invoke-WidSync
 
     if ( -not $force )
     {
-        Write-Output "You must use the 'Force' parameter" -ForegroundColor Yellow
+        Write-Host "You must use the 'Force' parameter" -ForegroundColor Yellow
         return
     }
 
@@ -52,27 +52,27 @@ function Invoke-WidSync
     {
         if ($LastSyncStatus -eq '0')
         {
-            Write-Output "Resetting the serialnumber column of ServiceStateSummary table to force a full WID sync" -ForegroundColor Green
+            Write-Host "Resetting the serialnumber column of ServiceStateSummary table to force a full WID sync" -ForegroundColor Green
         
-            Write-Output "ServiceStateSummary table content before reset:" -ForegroundColor Green
+            Write-Host "ServiceStateSummary table content before reset:" -ForegroundColor Green
             Get-AdfsWidServiceStateSummary
 
-            Write-Output "Resetting the serialnumber of ServiceStateSummary table" -ForegroundColor Green
+            Write-Host "Resetting the serialnumber of ServiceStateSummary table" -ForegroundColor Green
             Reset-AdfsWidServiceStateSummarySerialNumbers
 
-            Write-Output "ServiceStateSummary table content after reset:" -ForegroundColor Green
+            Write-Host "ServiceStateSummary table content after reset:" -ForegroundColor Green
             Get-AdfsWidServiceStateSummary
 
-            Write-Output "The full sync will occur on this AD FS Secondary server during the next normal sync poll (by default it occurs every 5 minutes)" -ForegroundColor Green
+            Write-Host "The full sync will occur on this AD FS Secondary server during the next normal sync poll (by default it occurs every 5 minutes)" -ForegroundColor Green
         } 
         else 
         {
-            Write-Output "The last sync status was not sucessful. Cannot force WID sync." -ForegroundColor Yellow
+            Write-Host "The last sync status was not sucessful. Cannot force WID sync." -ForegroundColor Yellow
         }
     }
     else
     {
-        Write-Output "This AD FS server is not a secondary server. Please run this cmdlet on your secondary server." -ForegroundColor Yellow
+        Write-Host "This AD FS server is not a secondary server. Please run this cmdlet on your secondary server." -ForegroundColor Yellow
     }
 }
 
