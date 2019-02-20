3f5a00bf682076f309bf7deb7ac137bf45c45213
ehunter1878 <durdur2@yahoo.com>
2019-02-20 11:11:45 -0800

    Fix TestProxySsslBidings missing thumbprint bug (#62)
    * Add logic for TestProxySslBindings to check if the test is being executed by Connect Health

--- diagnosticsModule/Private/TestUtilities.ps1
+++ diagnosticsModule/Private/TestUtilities.ps1
@@ -272,7 +272,12 @@ Function TestAdfsProxyHealth()
 
     if ([string]::IsNullOrWhiteSpace($sslThumbprint))
     {
-        $functionsToRun += "TestProxySslBindings";
+        # Attempt to load first the synthetic transactions library to test if Connect Health is the executer of the script
+        ipmo .\Microsoft.Identity.Health.Adfs.SyntheticTransactions.dll -ErrorAction SilentlyContinue -ErrorVariable synthTxErrVar
+        if ($synthTxErrVar -ne $null)
+        {
+            $functionsToRun += "TestProxySslBindings"
+        }
     }
     else
     {
