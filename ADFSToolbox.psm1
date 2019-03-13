58a5ff155b9971ab24d0a9e1dc6780182bb054ce
Madhav Patel <patel.v.madhav@gmail.com>
2019-03-13 11:38:11 -0700

    Fixing bug due to missing New-Guid cmdlet in PS 4.0 (#67)
    * Fixing bug from missing New-Guid cmdlet in PS 4.0

--- ADFSToolbox.psm1
+++ ADFSToolbox.psm1
@@ -20,7 +20,7 @@
     Copyright (c) Microsoft Corporation. All rights reserved.
 #>
 
-New-Variable -Name ModuleVersion -Value "1.0.12"
+New-Variable -Name ModuleVersion -Value "1.0.13"
 
 $url = "https://api.github.com/repos/Microsoft/adfsToolbox/releases/latest"
 $oldProtocol = [Net.ServicePointManager]::SecurityProtocol
